fdb9ecc5c3e3b1c63986eccf8676c381c528604b
kt0811 <hansam2396@gmail.com>
2022-01-14 00:32:10 +0900

    그리디3-숫자 카드 게임

---
+++ coding_Test_cpp/1_3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, m;
+	int result = 0;
+
+	cin >> n >> m;
+
+	for (int i = 0; i < n; ++i)
+	{
+		int min_value = 10001;
+
+		for (int j = 0; j < m; ++j)
+		{
+			int value = 0;
+			cin >> value;
+			min_value = min(value, min_value);
+		}
+
+		result = max(result, min_value);
+	}
+
+	cout << result << endl;
+
+	return 0;
+}
