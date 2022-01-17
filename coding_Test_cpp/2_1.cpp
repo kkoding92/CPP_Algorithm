ded00edb723a2610441348830676adff2bc87c62
kt0811 <hansam2396@gmail.com>
2022-01-17 15:50:24 +0900

    구현1 - 상하좌우

---
+++ coding_Test_cpp/2_1.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct POINT
+{
+	int x, y;
+};
+
+POINT curPoint = { 1, 1 };
+
+void main()
+{
+	int input;
+	string plan;
+
+	cin >> input;
+	cin.ignore();
+	getline(cin, plan);
+
+	for (int i = 0; i < plan.size(); ++i)
+	{
+		POINT movePoint = {0, 0};
+		POINT tempPoint;
+
+		switch (plan[i])
+		{
+		case 'L':
+			movePoint = { 0, -1 };
+			break;
+		case 'R':
+			movePoint = { 0, 1 };
+			break;
+		case 'U':
+			movePoint = { -1, 0 };
+			break;
+		case 'D':
+			movePoint = { 1, 0 };
+			break;
+		}
+
+		tempPoint.x = curPoint.x + movePoint.x;
+		tempPoint.y = curPoint.y + movePoint.y;
+
+		if (tempPoint.x < 1 || tempPoint.x > input || tempPoint.y < 1 || tempPoint.y > input)
+			continue;
+
+		curPoint.x = tempPoint.x;
+		curPoint.y = tempPoint.y;
+	}
+
+	cout << curPoint.x << " " << curPoint.y << endl;
+}
