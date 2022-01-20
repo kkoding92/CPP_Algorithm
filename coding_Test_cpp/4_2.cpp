5cb7709dfc61ac7c8f943d749e07532d63e08601
kt0811 <hansam2396@gmail.com>
2022-01-20 20:53:37 +0900

    정렬2_삽입

--- coding_Test_cpp/4_2.cpp
+++ coding_Test_cpp/4_2.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 using namespace std;
 
-// 선택 정렬
+// 삽입 정렬
 int main(void)
 {
 	int n = 10;
 	int arr[10] = { 7, 5, 9, 0, 3, 1, 6, 2, 4, 8 };
 
-	for (int i = 0; i < n; ++i)
+	for (int i = 1; i < n; ++i)
 	{
-		int min_index = i;
-
-		for (int j = i + 1; j < n; ++j)
+		for (int j = i; j > 0; --j)
 		{
-			if (arr[min_index] > arr[j])
-				min_index = j;
+			if (arr[j] < arr[j - 1])
+				swap(arr[j], arr[j - 1]);
+			else
+				break;
 		}
-
-		swap(arr[i], arr[min_index]);
 	}
 
 	for (int i = 0; i < n; ++i)
