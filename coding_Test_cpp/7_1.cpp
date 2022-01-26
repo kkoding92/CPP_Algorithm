65e353598b6ca381b814a6b64ec7ecbf8fcfb358
kt0811 <hansam2396@gmail.com>
2022-01-26 18:20:14 +0900

    최단경로_다익스트라

--- coding_Test_cpp/7_1.cpp
+++ coding_Test_cpp/7_1.cpp
@@ -1,35 +1,81 @@
 #include <iostream>
 #include <vector>
+
+#define INF 1e9 // 무한을 의미하는 값으로 10억을 설정
 using namespace std;
 
-int n, m;
-vector<int> arr;
+// 노드의 개수(N), 간선의 개수(M), 시작 노드 번호(Start)
+// 노드의 개수는 최대 100,000개라고 가정
+int n, m, start;
+// 각 노드에 연결되어 있는 노드에 대한 정보를 담는 배열
+vector<pair<int, int> > graph[100001];
+// 방문한 적이 있는지 체크하는 목적의 배열 만들기
+bool visited[100001];
+// 최단 거리 테이블 만들기
+int d[100001];
+
+// 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+int getSmallestNode() {
+    int min_value = INF;
+    int index = 0; // 가장 최단 거리가 짧은 노드(인덱스)
+    for (int i = 1; i <= n; i++) {
+        if (d[i] < min_value && !visited[i]) {
+            min_value = d[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+void dijkstra(int start) 
+{
+	// 시작 노드에 대해서 초기화
+	d[start] = 0;
+	visited[start] = true;
+	for (int i = 0; i < graph[start].size(); ++i)
+		d[graph[start][i].first] = graph[start][i].second;
+
+    // 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for (int i = 0; i < n - 1; ++i)
+    {
+        int now = getSmallestNode();
+        visited[now] = true;
+        // 현재 노드와 연결된 다른 노드를 확인
+        for (int j = 0; j < graph[now].size(); j++) 
+        {
+            int cost = d[now] + graph[now][j].second;
+            // 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if (cost < d[graph[now][j].first]) {
+                d[graph[now][j].first] = cost;
+            }
+        }
+    }
+}
 
 int main(void)
 {
-	cin >> n >> m;
-	for (int i = 0; i < n; ++i)
+	cin >> n >> m >> start;
+
+	for (int i = 0; i < m; ++i)
 	{
-		int temp;
-		cin >> temp;
-		arr.push_back(temp);
+		int a, b, c;
+		cin >> a >> b >> c;
+		// a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+		graph[a].push_back({b, c});
 	}
 
-	// 한 번 계산된 결과를 저장하기 위한 DP 테이블 초기화
-	vector<int> d(m + 1, 10001);
+	// 최단 거리 테이블을 모두 무한으로 초기화
+	fill_n(d, 100001, INF);
 
-	d[0] = 0;
-	for (int i = 0; i < n; ++i)
-	{
-		for (int j = arr[i]; j <= m; ++j)
-		{
-			if (d[j - arr[i]] != 10001)
-				d[j] = min(d[j], d[j - arr[i]] + 1);
-		}
-	}
+    // 다익스트라 알고리즘을 수행
+    dijkstra(start);
 
-	if (d[m] == 10001)
-		cout << -1 << '\n';
-	else
-		cout << d[m] << "\n";
+    // 모든 노드로 가기 위한 최단 거리를 출력
+    for (int i = 1; i <= n; i++)
+    {
+        if (d[i] == INF) 
+            cout << "INFINITY" << '\n';
+        else 
+            cout << d[i] << '\n';
+    }
 }
