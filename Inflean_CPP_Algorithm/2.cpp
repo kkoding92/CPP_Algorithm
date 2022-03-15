20ab7ad40bf8a5bf4e9aa5811a118ff739d533b1
kt0811 <hansam2396@gmail.com>
2022-03-15 21:04:16 +0900

    it_2

---
+++ Inflean_CPP_Algorithm/2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a, b, sum = 0;
+
+	cin >> a >> b;
+
+	for (int i = a; i <= b; ++i)
+	{
+		cout << i;
+		sum = sum + i;
+
+		if (i == b)
+			cout << " = " << sum;
+		else
+			cout << " + ";
+	}
+}
