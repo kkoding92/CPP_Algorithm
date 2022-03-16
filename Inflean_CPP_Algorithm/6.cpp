b272e5f7d30fd010d1acfd288b2bf98f0bc39ea2
kt0811 <hansam2396@gmail.com>
2022-03-17 00:44:52 +0900

    fix - it_6

--- Inflean_CPP_Algorithm/6.cpp
+++ Inflean_CPP_Algorithm/6.cpp
@@ -8,31 +8,20 @@ int main()
 	cin >> str;
 
 	int iSize = str.size();
-	string value = "";
+	int value = 0;
 
 	for (int i = 0; i < iSize; ++i)
 	{
 		if (isdigit(str[i]))
-		{
-			if (str[i] - '0' == 0)
-			{
-				if (value.size() == 0)
-					continue;
-				else
-					value = value + str[i];
-			}
-			else
-				value = value + str[i];
-		}
+			value = value * 10 + (str[i] - '0');
 	}
 
 	cout << value << '\n';
 
-	int temp = stoi(value);
 	int cnt = 0;
-	for (int i = 1; i <= temp; ++i)
+	for (int i = 1; i <= value; ++i)
 	{
-		if (temp % i == 0)
+		if (value % i == 0)
 			cnt = cnt + 1;
 	}
 
